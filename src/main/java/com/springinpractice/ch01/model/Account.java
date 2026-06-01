62b8d99cc8d9a72d4058ad2fa15c81b63e5404f4
Yubin <leepmh674735@gmail.com>
2026-06-01 19:11:00 +0900

    12

--- src/main/java/com/springinpractice/ch01/model/Account.java
+++ src/main/java/com/springinpractice/ch01/model/Account.java
@@ -1,23 +1,23 @@
 package com.springinpractice.ch01.model;
 
 import java.math.BigDecimal;
-import java.sql.Date;
-
-import javax.xml.crypto.Data;
+import java.util.Date; // java.util.Date로 통일하여 사용합니다.
 
 public class Account {
 	
 	private String accountNo;
-	private BigDacimal balance;
+	private BigDecimal balance; // 오타 수정 (BigDacimal -> BigDecimal)
 	private Date lastPaidOn;
 	
-	public Account(String accountNo, BigDecimal balance , Data lastPaidOn) {
-		this accountNo = accountNo;
+	// 생성자 매개변수 타입을 java.util.Date로 수정
+	public Account(String accountNo, BigDecimal balance, Date lastPaidOn) {
+		this.accountNo = accountNo; // 점(.) 누락 수정
 		this.balance = balance;
 		this.lastPaidOn = lastPaidOn;
 	}
 	
-	public String getAccountNO() {
+	// 메서드명 CamelCase 적용 (getAccountNO -> getAccountNo)
+	public String getAccountNo() {
 		return accountNo;
 	}
 	
